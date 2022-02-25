dcf430eff4d730e8d635569f73c2792dec80b695
Robert Sainton <robertsainton@outlook.com>
2022-02-24 20:25:01 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(){
+    const firstTwo = drivers.slice(0,2);
+    return firstTwo;
+}
+const returnLastTwoDrivers = function(){
+    const lastTwo = drivers.slice(2,4);
+    return lastTwo;
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(fare){
+         return function (){return fare * fare; } ;
+}
+function fareDoubler(fare){
+        return fare * 2;
+}
+function fareTripler(fare){
+    return fare * 3;
+}
+function selectDifferentDrivers(drivers, func){
+    const result =[] ;
+          if ( func === returnLastTwoDrivers){ const value = returnLastTwoDrivers();
+            result.push(value);
+             }
+    else if ( func === returnFirstTwoDrivers){ const value = returnFirstTwoDrivers();
+        result.push(value) ;
+          }
+          const finalResult = result.shift();
+                 return finalResult;
+          }                
